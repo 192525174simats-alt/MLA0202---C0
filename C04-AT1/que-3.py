6838b761dd16c1117cb8f37260615b5aa865ae1b
192525174simats-alt <192525174.simats@saveetha.com>
2026-08-18 11:15:07 +0530

    Update que-3.py

--- C04-AT1/que-3.py
+++ C04-AT1/que-3.py
@@ -1,1 +1,37 @@
+import numpy as np
 
+image = np.array([
+    [100, 105, 110, 115],
+    [102, 250, 108, 112],
+    [98, 103, 107, 111],
+    [95, 100, 105, 110]
+], dtype=float)
+
+updated = image.copy()
+
+rows, cols = image.shape
+
+for i in range(rows):
+    for j in range(cols):
+
+        neighbors = []
+
+        if i > 0:
+            neighbors.append(image[i - 1][j])
+
+        if i < rows - 1:
+            neighbors.append(image[i + 1][j])
+
+        if j > 0:
+            neighbors.append(image[i][j - 1])
+
+        if j < cols - 1:
+            neighbors.append(image[i][j + 1])
+
+        updated[i][j] = sum(neighbors) / len(neighbors)
+
+print("Original Image:")
+print(image)
+
+print("\nUpdated Image:")
+print(np.round(updated, 2))
